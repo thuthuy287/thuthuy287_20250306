6af886593bd680d979d7fb2f0eb17c0fa18ef4ff
thuthuy287 <thuthuyvu287@gmail.com>
2025-06-17 19:03:23 +0900

    Update SetupInfosCADDataTool_0423_1200.py

--- SetupInfosCADDataTool_0423_1200.py
+++ SetupInfosCADDataTool_0423_1200.py
@@ -3,193 +3,143 @@ import os
 import ansa
 from ansa import *
 
-import math
-
-def CreateMorphingBoxFunc():
+deck = constants.LSDYNA
+@session.defbutton('1_CAD ASSEMBLY', 'DividePartTool','Tách các chi tiết đang bị gộp lại thành 1 part')
+def DividePartTool():
 	# Need some documentation? Run this with F5
-	AllsFaces = base.PickEntities(constants.LSDYNA, ['FACE'])
-	if AllsFaces != None:
-		DictIsolateFaceConnect = base.IsolateConnectivityGroups(entities = AllsFaces, separate_at_blue_bounds = False, separate_at_pid_bounds = True)
-		if DictIsolateFaceConnect != None:
-			for KeyNameGroupFace, FacesSelect in DictIsolateFaceConnect.items():
-				NewHexaBoxs = mesh.HexaBoxOrtho(loaded_elements = FacesSelect, min_flag = True)
-				if NewHexaBoxs != 0:
-					HexaBoxFaces = base.CollectEntities(constants.LSDYNA, NewHexaBoxs, ['HEXA_BOX_FACE'])
-					if len(HexaBoxFaces) >0:
-						ListBoxFaceDouble = FindBoxFaceDoubleVecsFunc(HexaBoxFaces)
-						if len(ListBoxFaceDouble) >0:
-							DictSingleBoxFace_ListEntity = FindEntityOnBoxFacesFunc(ListBoxFaceDouble, FacesSelect)
-							if len(DictSingleBoxFace_ListEntity) >0:
-								AlignBoxFaceToCadFunc(DictSingleBoxFace_ListEntity)
-					
-			base.RedrawAll()
-
-###-------------------------------- Align box ve cad----------------------------------------
-def AlignBoxFaceToCadFunc(DictSingleBoxFace_ListEntity):
+	TopWindow = guitk.BCWindowCreate("Divide Many Element On Single Part", guitk.constants.BCOnExitDestroy)
 	
-	for SingleBoxConnect, ListEntityFaceConnect in DictSingleBoxFace_ListEntity.items():
-		ListFaceExtends = ExtendsFacesOnMorphFunc(ListEntityFaceConnect)
-		if len(ListFaceExtends) >0:
-			ValsBoxFaceConnect = base.GetEntityCardValues(constants.LSDYNA, SingleBoxConnect, ['Name', 'Comment'])
-			TokensNameBoxConnect = ValsBoxFaceConnect['Name'].split('_')
-			VecsOfBoxConnect = [float(TokensNameBoxConnect[0]), float(TokensNameBoxConnect[1]), float(TokensNameBoxConnect[2])]
-			mesh.AssociateBoxFacesToSurfs(box_faces = [SingleBoxConnect], surface_ents = ListFaceExtends, user_projection_mode_vector = VecsOfBoxConnect, remove_points = False)
+	BCButtonGroup_1 = guitk.BCButtonGroupCreate(TopWindow, "Select Options:", guitk.constants.BCHorizontal)
+	BCRadioButton_1 = guitk.BCRadioButtonCreate(BCButtonGroup_1, "Auto", None, 0)
+	BCRadioButton_2 = guitk.BCRadioButtonCreate(BCButtonGroup_1, "Manual", None, 0)
+	guitk.BCRadioButtonSetChecked(BCRadioButton_1, True)
+	
+	BCProgressBar_1 = guitk.BCProgressBarCreate(TopWindow, 100)
+	BCLabel_1 = guitk.BCLabelCreate(TopWindow, "")
+	BCDialogButtonBox_1 = guitk.BCDialogButtonBoxCreate(TopWindow)
+	
+	_window = [BCProgressBar_1, BCLabel_1, BCRadioButton_1, BCRadioButton_2]
+	guitk.BCWindowSetRejectFunction(TopWindow, _Reject_Button, _window)
+	guitk.BCWindowSetAcceptFunction(TopWindow, _Accept_Button, _window)
+	
+	guitk.BCShow(TopWindow)
+
+def _Reject_Button(TopWindow, _window):
+	
+	return 1
+
+def _Accept_Button(TopWindow, _window):
+	
+	Probar = _window[0]
+	Label = _window[1]
+	AutoStatus = guitk.BCRadioButtonIsChecked(_window[2])
+	ManualStatus = guitk.BCRadioButtonIsChecked(_window[3])
+	
+	PropsSelected = []
+	if AutoStatus == True:
+		PropsSelected = base.CollectEntities(deck, None, ['SECTION_SHELL'])
+		PartsSelected = base.CollectEntities(deck, None, ['ANSAPART'])
+	if ManualStatus == True:
+		PropsSelected = base.PickEntities(deck, ['SECTION_SHELL'])
+		PartsSelected = base.CollectEntities(deck, None, ['ANSAPART'], filter_visible = True)
+	
+	if len(PropsSelected) >0:
+		DivideElementOnPart(PropsSelected, Probar, Label)
+		DivideEntityPartsFunc(PartsSelected, Probar, Label)
+	
+	base.RedrawAll()
+
+
+def DivideEntityPartsFunc(PartsSelected, Probar, Label):
+	
+	guitk.BCProgressBarReset(Probar)
+	guitk.BCProgressBarSetTotalSteps(Probar, len(PartsSelected))
+	for i in range(0, len(PartsSelected), 1):
+		guitk.BCLabelSetText(Label, 'Loading Parts Ids: ' + str(PartsSelected[i]._id))
 		
-			base.DeleteEntity(ListFaceExtends, True)
+		ValsPartsSelect = PartsSelected[i].get_entity_values(deck, ['PID', 'Name'])
+		TokensStringPID = ValsPartsSelect['PID'].split(',')
 		
-####-------------------------------- Mo rong cac faces voi khoang cach la 10 ----------------------------------------	
-def ExtendsFacesOnMorphFunc(ListEntityFaceConnect):
+		if len(TokensStringPID) >1:
+			for k in range(1, len(TokensStringPID), 1):
+				PropsDivides = base.GetEntity(deck, 'SECTION_SHELL', int(TokensStringPID[k]))
+				if PropsDivides != None:
+					FacesOnPropsDivide = base.CollectEntities(deck, PropsDivides, ['FACE'])
+					if len(FacesOnPropsDivide) >0:
+						NamePartDivide = ValsPartsSelect['Name'] + '@' + str(FacesOnPropsDivide[0]._id)
+						ModulePartDivide = PropsDivides._id
 	
-	ListFaceExtends = []
-	for i in range(0, len(ListEntityFaceConnect), 1):
-		ElemsFaceExtend = base.SurfaceExtend(face = ListEntityFaceConnect[i], extend_value = 10, untrim = True, delete_original_face = False)
-		if ElemsFaceExtend != None:
-			ListFaceExtends.append(ElemsFaceExtend)
+						ListGroupsOld = FindLocationOfPartsOldsFunc(FacesOnPropsDivide)
+						EntityPartDivide = CreateNewEntityParts(NamePartDivide, ModulePartDivide)
+						if EntityPartDivide != None:
+							EntityPartDivide.set_entity_values(deck, {'Module Id': ModulePartDivide})
+							base.SetEntityPart(FacesOnPropsDivide, EntityPartDivide)
+							if ListGroupsOld != None:
+								base.SetEntityPart(EntityPartDivide, ListGroupsOld[0])
+				
+		guitk.BCProgressBarSetProgress(Probar, i+1)
+		
+def DivideElementOnPart(PropsSelected, Probar, Label):
 	
-	return ListFaceExtends		
-
-###-------------------------------- Tim cac face cad cung vector voi morph faces----------------------------------------			
-def FindEntityOnBoxFacesFunc(ListBoxFaceDouble, FacesSelect):
+	guitk.BCProgressBarReset(Probar)
+	guitk.BCProgressBarSetTotalSteps(Probar, len(PropsSelected))
 	
-	DictSingleBoxFace_ListEntity = {}
-	for i in range(0, len(ListBoxFaceDouble), 1):
-		ListEntityOnBoxFace = CheckVecsOfBoxWithVecsOfFacesSelectFunc(ListBoxFaceDouble[i], FacesSelect)
-		if len(ListEntityOnBoxFace) >0:
-			FindFacesNearestBoxFunc(ListBoxFaceDouble[i], ListEntityOnBoxFace, DictSingleBoxFace_ListEntity)
-	
-	return DictSingleBoxFace_ListEntity
-#	if len(DictSingleBoxFace_ListEntity) >0:
-#		for aa, bb in DictSingleBoxFace_ListEntity.items():
-#			setFace = base.CreateEntity(constants.LSDYNA, 'SET')
-#			base.AddToSet(setFace, bb)		
-
-####-------------------------------- Check khoang cach cua face toi tung box face----------------------------------------
-def FindFacesNearestBoxFunc(ListBoxFaceDistance, ListEntityOnBoxFace, DictSingleBoxFace_ListEntity):
-	
-	DictIsolateGroupFace = base.IsolateConnectivityGroups(entities = ListEntityOnBoxFace, separate_at_blue_bounds = False, separate_at_pid_bounds = True)
-	if DictIsolateGroupFace != None:
-		for i in range(0, len(ListBoxFaceDistance), 1):
-			ValsBoxFaceDistance = base.GetEntityCardValues(constants.LSDYNA, ListBoxFaceDistance[i], ['Name', 'Comment'])
-			TokensCommentBoxDistance = ValsBoxFaceDistance['Comment'].split('_')
-			AxisCogOfBoxFace = [float(TokensCommentBoxDistance[0]), float(TokensCommentBoxDistance[1]), float(TokensCommentBoxDistance[2])]
+	for i in range(0, len(PropsSelected), 1):
+		guitk.BCLabelSetText(Label, 'Loading Props Ids: ' + str(PropsSelected[i]._id))
+		FacesOnProps = base.CollectEntities(deck, PropsSelected[i], ['FACE'])
+		if len(FacesOnProps) >0:
+			ListFacesIsolate = IsolateEntiyOnProps(FacesOnProps)
+			if len(ListFacesIsolate) >1:
+				SetupInfosToFacesIsolate(ListFacesIsolate, PropsSelected[i])
 			
-			ListDistanceCalculate = []
-			ListFacesConnect = []
-			for KeyGroupIsolate, ListFacesIsolate in DictIsolateGroupFace.items():
-				ProjContainerResult = calc.ProjectPointToContainer(AxisCogOfBoxFace, ListFacesIsolate)
-				ListDistanceCalculate.append(ProjContainerResult[1])
-				ListFacesConnect.append(ListFacesIsolate)
-			
-			if len(ListDistanceCalculate) >0:
-				IndexMinDistance = ListDistanceCalculate.index(min(ListDistanceCalculate))
-				DictSingleBoxFace_ListEntity[ListBoxFaceDistance[i]] = ListFacesConnect[IndexMinDistance]
+		guitk.BCProgressBarSetProgress(Probar, i+1)
 
-####-------------------------------- Check vector cua face trung voi vector cua tung box face----------------------------------------
-def CheckVecsOfBoxWithVecsOfFacesSelectFunc(ListBoxFacesDefine, FacesSelect):
+def SetupInfosToFacesIsolate(ListFacesIsolate, PropsIsolate):
 	
-	ListEntityOnBoxFace = []
-	for i in range(0, len(ListBoxFacesDefine), 1):
-#		print(ListBoxFacesDefine[i]._id)
-		ValsBoxFaceDefine = base.GetEntityCardValues(constants.LSDYNA, ListBoxFacesDefine[i], ['Name', 'Comment'])
-		TokensNameBoxDefine = ValsBoxFaceDefine['Name'].split('_')
-		VecsOfBoxFaceDefine = [float(TokensNameBoxDefine[0]), float(TokensNameBoxDefine[1]), float(TokensNameBoxDefine[2])]
-		if len(VecsOfBoxFaceDefine) >0:
-			for k in range(0, len(FacesSelect), 1):
-				OrientsVecOfFace = base.GetFaceOrientation(FacesSelect[k])
-				AngleTwoVecs = math.degrees(calc.CalcAngleOfVectors(VecsOfBoxFaceDefine, OrientsVecOfFace))
-				if AngleTwoVecs < 5 or AngleTwoVecs > 175:
-#					print(FacesSelect[k]._id)
-					ListEntityOnBoxFace.append(FacesSelect[k])
-	
-	return ListEntityOnBoxFace
-	
-###-------------------------------- Tim cac hexa box face co vector trung nhau----------------------------------------	
-def FindBoxFaceDoubleVecsFunc(HexaBoxFaces):
-	
-	ListBoxFaceDouble = []
-	GetVectorOfMorphFacesFunc(HexaBoxFaces)
-	
-	ListHexaBoxFacesCheck = []
-	for i in range(0, len(HexaBoxFaces), 1):
-		PosSeachBoxFace1st = FindEntityInListElemsFunc(HexaBoxFaces[i], ListHexaBoxFacesCheck)
-		if PosSeachBoxFace1st == None:
-			ValsBoxFace1st = base.GetEntityCardValues(constants.LSDYNA, HexaBoxFaces[i], ['Name', 'Comment'])
-			TokensNameBox1st = ValsBoxFace1st['Name'].split('_')
-			VecsOfBoxFace1st = [float(TokensNameBox1st[0]), float(TokensNameBox1st[1]), float(TokensNameBox1st[2])]
-			for k in range(0, len(HexaBoxFaces), 1):
-				if HexaBoxFaces[k] != HexaBoxFaces[i]:
-					ValsBoxFace2nd = base.GetEntityCardValues(constants.LSDYNA, HexaBoxFaces[k], ['Name', 'Comment'])
-					TokensNameBox2nd = ValsBoxFace2nd['Name'].split('_')
-					VecsOfBoxFace2nd = [float(TokensNameBox2nd[0]), float(TokensNameBox2nd[1]), float(TokensNameBox2nd[2])]
-					
-					Angle2Vecs = math.degrees(calc.CalcAngleOfVectors(VecsOfBoxFace1st, VecsOfBoxFace2nd))
-					if Angle2Vecs < 5 or Angle2Vecs >175:
-						ListHexaBoxFacesCheck.append(HexaBoxFaces[k])
-						ListBoxFaceDouble.append([HexaBoxFaces[i], HexaBoxFaces[k]])
-	
-	return ListBoxFaceDouble
+	for i in range(1, len(ListFacesIsolate), 1):
+		PropsFaceIsolate = base.CopyEntity(None, PropsIsolate)
+		PropsFaceIsolate.set_entity_values(deck, {'User/cad_material': PropsIsolate._id})
+		for k in range(0, len(ListFacesIsolate[i]), 1):
+			ListFacesIsolate[i][k].set_entity_values(deck, {'PID': PropsFaceIsolate._id})
+		
+		NamePartFaceIsolate = PropsFaceIsolate._name
+		ModulePartFaceIsolate = PropsFaceIsolate._id
+		
+		ListGroupOfPartsOld = FindLocationOfPartsOldsFunc(ListFacesIsolate[i])
+		
+		EntityPartFaceIsolate = CreateNewEntityParts(NamePartFaceIsolate, ModulePartFaceIsolate)
+		if EntityPartFaceIsolate != None:
+			EntityPartFaceIsolate.set_entity_values(deck, {'Module Id': ModulePartFaceIsolate})
+			base.SetEntityPart(ListFacesIsolate[i], EntityPartFaceIsolate)
+			if ListGroupOfPartsOld != None:
+				base.SetEntityPart(EntityPartFaceIsolate, ListGroupOfPartsOld[0])
 
-#####-------------------------------- Tim vector cua morph faces----------------------------------------		
-def GetVectorOfMorphFacesFunc(HexaBoxFaces):
+def FindLocationOfPartsOldsFunc(EntityReference):
 	
-	for k in range(0, len(HexaBoxFaces), 1):
-		HexaBoxEdges = base.CollectEntities(constants.LSDYNA, HexaBoxFaces[k], ['HEXA_BOX_EDGE'])
-		CurveOnHexaBoxEdges = base.CurvesFromEntities(entities = HexaBoxEdges, connect_curves = [0.05, 45])
-		if len(CurveOnHexaBoxEdges) >0:
-			ListAxisPointOnCurveEdge = []
-			for i in range(0, len(CurveOnHexaBoxEdges), 1):
-				ValsCurveHexaBoxEdge = base.GetEntityCardValues(constants.LSDYNA, CurveOnHexaBoxEdges[i], ['Start X', 'Start Y', 'Start Z', 'End X', 'End Y', 'End Z'])
-				CogNodesOfCurve = [(ValsCurveHexaBoxEdge['Start X'] + ValsCurveHexaBoxEdge['End X'])/2, (ValsCurveHexaBoxEdge['Start Y'] + ValsCurveHexaBoxEdge['End Y'])/2, (ValsCurveHexaBoxEdge['Start Z'] + ValsCurveHexaBoxEdge['End Z'])/2]
-				ListAxisPointOnCurveEdge.append(CogNodesOfCurve)
-			
-			if len(ListAxisPointOnCurveEdge) >0:
-				AxisNodesCurve1st = ListAxisPointOnCurveEdge[0]
-				AxisNodesCurve2nd = ListAxisPointOnCurveEdge[1]
-				AxisNodesCurve3rd = ListAxisPointOnCurveEdge[2]
-				NormalDirection = CreatePlaneOnThreePointsFunc(AxisNodesCurve1st, AxisNodesCurve2nd, AxisNodesCurve3rd)
-				ListCogPoints = CreateCogPointsFunc(ListAxisPointOnCurveEdge)
-				base.SetEntityCardValues(constants.LSDYNA, HexaBoxFaces[k], {'Name': str(NormalDirection[0]) + '_' + str(NormalDirection[1]) + '_' + str(NormalDirection[2]), 'Comment': str(ListCogPoints[0]) + '_' + str(ListCogPoints[1]) + '_' + str(ListCogPoints[2])})
-			
-		base.DeleteEntity(CurveOnHexaBoxEdges, True)
+	ListGroupReference = []
+	PartReference = base.GetEntityPart(EntityReference[0])
+	if PartReference != None:
+		ValsPartRefer = PartReference.get_entity_values(deck, ['Hierarchy'])
+		TokensHierarchy = ValsPartRefer['Hierarchy'].split('/')
+		NameGroupPartRefer = TokensHierarchy[len(TokensHierarchy) -2]
+		ListGroupReference = base.NameToEnts(pattern = NameGroupPartRefer, deck = deck, match = constants.ENM_EXACT)
+	
+	return ListGroupReference
+	
+def IsolateEntiyOnProps(FacesOnProps):
+	ListFacesIsolate = []
+	
+	GroupFaceIsolate = base.IsolateConnectivityGroups(entities = FacesOnProps, separate_at_blue_bounds = True, separate_at_pid_bounds = True)
+	if GroupFaceIsolate != None:
+		for NameGroupIsolate, EntityFaceIsolate in GroupFaceIsolate.items():
+			ListFacesIsolate.append(EntityFaceIsolate)
+	
+	return ListFacesIsolate
 
-########################### Help Function ##################################
-#####-------------------------------- Tao Cog cua list point----------------------------------------	
-def CreateCogPointsFunc(ListAxisPoints):
+def CreateNewEntityParts(NamePartFaceIsolate, ModulePartFaceIsolate):
 	
-	ListAxisX = []
-	ListAxisY = []
-	ListAxisZ = []
-	for i in range(0, len(ListAxisPoints), 1):
-		ListAxisX.append(ListAxisPoints[i][0])
-		ListAxisY.append(ListAxisPoints[i][1])
-		ListAxisZ.append(ListAxisPoints[i][2])
-	
-	AxisPointsCog = [sum(ListAxisX)/len(ListAxisX), sum(ListAxisY)/len(ListAxisY), sum(ListAxisZ)/len(ListAxisZ)]
-	
-	return AxisPointsCog
+	EntiyPart = base.NewPart(NamePartFaceIsolate + '@' + str(ModulePartFaceIsolate))
+		
+	return EntiyPart
 
-#####-------------------------------- Tao mat phang di qua 3 diem cho truoc----------------------------------------			
-def CreatePlaneOnThreePointsFunc(A, B, C):
-	# Tinh vector AB
-	AB = [B[0] - A[0], B[1] - A[1], B[2] - A[2]]
-	# Tinh vector AC
-	AC = [C[0] - A[0], C[1] - A[1], C[2] - A[2]]
-	# Tinh tich co huong cua hai vector (tim vector phap tuyen cua mat phang) a=(a1;a2;a3 ) và b=(b1;b2;b3 )
-	#				[a, b] = (a2b3 - a3b2, a3b1 - a1b3, a1b2 - a2b1)							
-	NormalDirection = [AB[1]*AC[2] - AB[2]*AC[1], AB[2]*AC[0] - AB[0]*AC[2], AB[0]*AC[1] - AB[1]*AC[0]]
-	
-	return NormalDirection
-
-#####--------------------------------- Tim phan tu trong 1 list------------------------------------------------------------
-def FindEntityInListElemsFunc(EntitySearch, ListElems):
-	
-	try:
-		Pos = ListElems.index(EntitySearch)
-	except:
-		Pos = None
-	else:
-		Pos = Pos
-	
-	return Pos
-	
-CreateMorphingBoxFunc()
+#DividePartTool()
